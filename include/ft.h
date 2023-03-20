4bd36635b1480a432ddc81718c700069db295cb6
Xael <97090524+XaelBaseth@users.noreply.github.com>
2023-03-20 13:13:32 +0100

    IloveDravem (#1)
    Co-authored-by: Axel Charlot <acharlot@c1r4p2.42lehavre.fr>

---
+++ include/ft.h
@@ -0,0 +1,30 @@
+#ifndef FT_H
+# define FT_H
+
+# define MAX_LINES	
+# define BUFFER_SIZE	
+# define MIN(A, B)	
+
+typedef struct str_point
+{
+	int	x;
+	int	y;
+}t_point;
+
+typedef struct str_map
+{
+	char	*content;
+	char	*matrix;
+	char	empty;
+	char	full;
+	char	obs;
+	t_point	size;
+}t_map;
+
+typedef struct str_square
+{
+	t_point	coords;
+	int	size;
+}t_square;
+
+#endif
